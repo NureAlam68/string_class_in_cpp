7ec2f672e574e2b22e6ffc454f8d89a20522e4bd
NureAlam68 <nurealam151068@gmail.com>
2025-07-14 21:06:18 +0600

    string capacity functions

--- string_capacity.cpp
+++ string_capacity.cpp
@@ -3,8 +3,21 @@ using namespace std;
 int main()
 {
     string s = "Hello World";
+    // s.clear();
     cout << s.size() << endl;
     cout << s.max_size() << endl;
     cout << s.capacity() << endl;
+    // s.resize(5);
+    s.resize(15,'m');
+    cout << s << endl;
+
+    if(s.empty() == true) 
+    {
+        cout << "Empty" << endl;
+    }
+    else
+    {
+        cout << "Not empty" << endl;
+    }
     return 0;
 }
