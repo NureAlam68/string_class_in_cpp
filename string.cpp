2b8a4f0b17f6481668eb7b08d0d26bedb400fc5a
NureAlam68 <nurealam151068@gmail.com>
2025-07-14 20:48:05 +0600

    string

---
+++ string.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Hello";
+    s = "My name is Nure Alam";
+
+    cout << s << endl;
+    return 0;
+}
+
+
+
+
+// using namespace std;
+// int main()
+// {
+//     string s = "Hello";
+//     string p = "Hello";
+
+//     if(s == p)
+//     {
+//         cout << "Same" << endl;
+//     }
+//     else
+//     {
+//         cout << "Not Same" << endl;
+//     }
+
+//     cout << s << endl;
+//     return 0;
+// }
