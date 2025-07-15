83ecedad19da93618920523ed2955225af6d9d78
NureAlam68 <nurealam151068@gmail.com>
2025-07-15 19:29:59 +0600

    string modifiers

---
+++ string_modifiers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Hi, Nur";
+    string p = ", Hello";
+    // s += p;
+    // s.append(p);
+    // s.push_back('O');
+    // s.pop_back();
+    // s = "Bye";
+    // s = p;
+    // s.assign(p);
+    // s.erase(2,1);
+    // s.replace(4, 3, "Sohag");
+    s.insert(7, "e Alam");
+    cout << s << endl;
+    return 0;
+}
